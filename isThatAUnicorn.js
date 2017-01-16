88f55789086874cb107cf5582ed6079b0f29c348
Karl <Karl@scc-wkit-clxa-4-221.scc.kit.edu>
2017-01-16 15:50:15 +0100

    Project uploaded
    Added all project files. introduction file is index.

---
+++ isThatAUnicorn.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+                  $(".picture").hide();
+                  
+                  var winCounter = 0;
+                  var ocounter = 1;
+                  var orientationStrings = ["South West", "North", "South East", "West", "South", "North West", "East", "North East"];
+                  var orientations = ["135", "0", "225", "90", "180", "45", "270", "315"];
+                  var accuracy = "20";
+                  var alpha = 0;
+
+                  
+                  $("#button").click(function() {
+                                   if(window.DeviceOrientationEvent) {
+                                     window.addEventListener('deviceorientation', function(event) {
+                                                alpha = event.alpha;
+                                                    }
+                                    }
+                                    
+                                    if(winCounter < 3 ) {
+                                                             winCounter++;
+                                                             $("#winCounter").text(winCounter);
+                                                             ocounter++;
+                                                             if (ocounter > 7) { ocounter = 0;};
+                                                             $("#direction").text(orientationStrings[ocounter]);
+                                                             
+                                                             $("#unicornreward").attr("src" , "unicornreward.gif");
+                                                             $("#unicornreward").fadeIn(250, function() {
+                                                                                        $("#unicornreward").delay(1900);
+                                                                                        $("#unicornreward").fadeOut(250);
+                                                                  
+                                                                                        });
+                                                             $("#unicornreward").attr("src" , "unicornreward.gif");
+                                     
+                                           }
+                                    else {
+                                                             winCounter= 0;
+                                                             $("#winCounter").text(winCounter);
+                                                             
+                                                             $("#unicornreward").attr("src" , "horsefail.gif");
+                                                             $("#unicornreward").fadeIn(250, function() {
+                                                                                        $("#unicornreward").delay(1900);
+                                                                                        $("#unicornreward").fadeOut(250);
+                                                                                        
+                                                                                        });
+                                                             $("#unicornreward").attr("src" , "horsefail.gif");
+                                          }
+                    });
+});
