4a76cab358a289f92c8eaf377e3b8cb1383de14c
Karl <Karl@scc-wkit-clxa-24-119.scc.kit.edu>
2017-01-17 14:46:59 +0100

    old atatus

--- isThatAUnicorn.js
+++ isThatAUnicorn.js
@@ -25,42 +25,44 @@ $(document).ready(function() {
                                         window.addEventListener('deviceorientation', function(event) {
                                                 alpha = event.alpha;
                                                 }, false);
+                                     
+                                     }
+                                     
+                                     waitforalpha(oldAlpha);
+                                     
                                      $("#alphaValue").text(alpha);
                                      
                                      
-                                     if(ocounter==1 && (360 - accuracy < alpha || alpha < accuracy) ||
-                                        ((orientations[ocounter] - accuracy) < alpha && alpha < (orientations[ocounter] + accuracy) ) ) {
-                                     winCounter++;
-                                     $("#winCounter").text(winCounter);
-                                     ocounter++;
-                                     if (ocounter > 7) { ocounter = 0;};
-                                     $("#direction").text(orientationStrings[ocounter]);
-                                     
-                                     $("#unicornreward").attr("src" , "unicornreward.gif");
-                                     $("#unicornreward").fadeIn(250, function() {
-                                                                $("#unicornreward").delay(1900);
-                                                                $("#unicornreward").fadeOut(250);
-                                                                
-                                                                });
-                                     $("#unicornreward").attr("src" , "unicornreward.gif");
-                                     
-                                     
-                                     }
-                                     else {
-                                     winCounter= 0;
-                                     $("#winCounter").text(winCounter);
-                                     
-                                     $("#unicornreward").attr("src" , "horsefail.gif");
-                                     $("#unicornreward").fadeIn(300, function() {
-                                                                $("#unicornreward").delay(1900);
-                                                                $("#unicornreward").fadeOut(250);
-                                                                
-                                                                });
-                                     $("#unicornreward").attr("src" , "horsefail.gif");
-                                     
-                                     }
-                                     }
+                                    if(ocounter==1 && (360 - accuracy < alpha || alpha < accuracy) ||
+                                       ((orientations[ocounter] - accuracy) < alpha && alpha < (orientations[ocounter] + accuracy) ) ) {
+                                                             winCounter++;
+                                                             $("#winCounter").text(winCounter);
+                                                             ocounter++;
+                                                             if (ocounter > 7) { ocounter = 0;};
+                                                             $("#direction").text(orientationStrings[ocounter]);
+                                                             
+                                                             $("#unicornreward").attr("src" , "unicornreward.gif");
+                                                             $("#unicornreward").fadeIn(250, function() {
+                                                                                        $("#unicornreward").delay(1900);
+                                                                                        $("#unicornreward").fadeOut(250);
+                                                                  
+                                                                                        });
+                                                             $("#unicornreward").attr("src" , "unicornreward.gif");
                                      
                                      
+                                           }
+                                    else {
+                                                             winCounter= 0;
+                                                             $("#winCounter").text(winCounter);
+                                     
+                                                             $("#unicornreward").attr("src" , "horsefail.gif");
+                                                             $("#unicornreward").fadeIn(300, function() {
+                                                                                        $("#unicornreward").delay(1900);
+                                                                                        $("#unicornreward").fadeOut(250);
+                                                                                        
+                                                                                        });
+                                                             $("#unicornreward").attr("src" , "horsefail.gif");
+                                                            
+                                          }
                     });
 });
